61f3c831dd7c5e53a6000e21200080c70a2acf0b
Akash Gupta <akashgupta7363@gmail.com>
2023-05-22 20:53:53 +0530

    config tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    fontFamily: {
+      Roboto: ["Roboto", "sans-serif"],
+      Poppins: ["Poppins", "sans-serif"],
+    },
+    extend: {
+      screens: {
+        "1000px": "1050px",
+        "1100px": "1110px",
+        "800px": "800px",
+        "1300px": "1300px",
+        "400px": "400px",
+      },
+    },
+  },
+  plugins: [],
+};
