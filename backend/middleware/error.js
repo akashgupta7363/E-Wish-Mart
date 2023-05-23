b7c8b7d45e4d687af0ae2869781b648c8db6771f
Akash Gupta <akashgupta7363@gmail.com>
2023-05-23 22:14:33 +0530

    added error handler

---
+++ backend/middleware/error.js
@@ -0,0 +1,32 @@
+const ErrorHandler = require("../utils/errorHandler");
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Inernal server error";
+
+  //wrong mongodb id error
+  if (err.name === "CastError") {
+    const message = `Resource not found with this id. Invalid ${err.path}`;
+    err = new ErrorHandler(message, 400);
+  }
+  if (err.code === 11000) {
+    const message = `Duplicate key${Object.keys(err.keyValue)} Entered`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // wrong jwt error
+  if (err.name === "JsonWebTokenError") {
+    const message = `Your url is invalid please try again later`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  //jwt expired
+  if (err.name === "TokenExpiredError") {
+    const message = `You url is expired please try again later`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  res.status(err.statusCode).json({
+    success: false,
+    message: err.message,
+  });
+};
