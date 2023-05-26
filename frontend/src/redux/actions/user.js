48c9621af7882906bdaeaee6edbc89434c78791f
Akash Gupta <akashgupta7363@gmail.com>
2023-05-26 11:40:24 +0530

    action and reducer added for loading user

---
+++ frontend/src/redux/actions/user.js
@@ -0,0 +1,23 @@
+import {} from "@reduxjs/toolkit";
+import axios from "axios";
+import { server } from "../../server";
+
+export const loadUser = () => async (dispatch) => {
+  try {
+    dispatch({
+      type: "LoadUserRequest",
+    });
+    const { data } = await axios.get(`${server}/user/getUser`, {
+      withCredentials: true,
+    });
+    dispatch({
+      type: "LoadUserSuccess",
+      payload: data.user,
+    });
+  } catch (error) {
+    dispatch({
+      type: "LoadUserFail",
+      payload: error.response.data.message,
+    });
+  }
+};
