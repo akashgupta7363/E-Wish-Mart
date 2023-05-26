26146b2b412fa4a2a27757ea01fae6323cabf2ab
Akash Gupta <akashgupta7363@gmail.com>
2023-05-26 21:25:53 +0530

    categories section done

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../components/header/Header.js";
 import Hero from "../components/Route/Hero/Hero.js";
+import Categories from "../components/Route/Categories/Categories.js";
 const HomePage = () => {
   return (
     <div>
       <Header activeHeading={1} />
       <Hero />
+      <Categories />
     </div>
   );
 };
--- frontend/src/static/data.js
+++ frontend/src/static/data.js
@@ -229,7 +229,7 @@ export const categoriesData = [
     title: "Gifts",
     subTitle: "",
     image_Url:
-      "https://securecdn.pymnts.com/wp-content/uploads/2014/11/Gifts-Photo-700x489.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2ObXeurtpOiFVn_TBNjwLeQs7YcrrFZdEBW55Oz-Jsg&s",
   },
   {
     id: 7,
