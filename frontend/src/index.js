d4d2c8890c1f0accfe8c42f331921e484224f8e7
Akash Gupta <akashgupta7363@gmail.com>
2023-05-26 11:39:42 +0530

    store of redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import { Provider } from "react-redux";
+import Store from "./redux/store";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  //<React.StrictMode>
-  <App />
-  //</React.StrictMode>
+  <Provider store={Store}>
+    <App />
+  </Provider>
 );
