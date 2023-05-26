093962539d7d3bebfc6442918c77122a4bec5595
Akash Gupta <akashgupta7363@gmail.com>
2023-05-26 17:43:47 +0530

    categories DropDown Added

--- frontend/src/components/header/DropDown.js
+++ frontend/src/components/header/DropDown.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "../../styles/style";
 
-const DropDown = () => {
-  return <div></div>;
+const DropDown = ({ categoriesData, setDropDown }) => {
+  const navigate = useNavigate();
+  const submiteHandler = (i) => {
+    navigate(`/products?category=${i.title}`);
+    setDropDown(false);
+    window.location.reload();
+  };
+  return (
+    <div className="pb-4 w-[270px] bg-[#fff] absolute z-30 rounded-b-md shadow-sm">
+      {categoriesData &&
+        categoriesData.map((i, index) => (
+          <div
+            key={index}
+            className={`${styles.normalFlex}`}
+            onClick={() => submiteHandler(i)}
+          >
+            <img
+              src={i.image_Url}
+              className="h-[25px] w-[25px] object-contain ml-[10px] select-none"
+            />
+            <h3 className="m-3 cursor-pointer select-none">{i.title}</h3>
+          </div>
+        ))}
+    </div>
+  );
 };
 
 export default DropDown;
