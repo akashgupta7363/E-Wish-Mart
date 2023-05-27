ced35155dad1980f3a891ed238c1dcfd17d4c3cc
Akash Gupta <akashgupta7363@gmail.com>
2023-05-27 11:18:39 +0530

    Product view card comp

--- frontend/src/components/Route/ProductDetailsCard/ProductDetailsCard.js
+++ frontend/src/components/Route/ProductDetailsCard/ProductDetailsCard.js
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
 import { RxCross1 } from "react-icons/rx";
 import styles from "../../../styles/style";
-import { AiOutlineMessage } from "react-icons/ai";
+import {
+  AiFillHeart,
+  AiOutlineHeart,
+  AiOutlineMessage,
+  AiOutlineShoppingCart,
+} from "react-icons/ai";
 
 const ProductDetailsCard = ({ open, setOpen, data }) => {
   const [count, setCount] = useState(1);
@@ -13,16 +18,16 @@ const ProductDetailsCard = ({ open, setOpen, data }) => {
     <div className="bg-white ">
       {data ? (
         <div className="fixed w-full h-screen top-0 left-0 bg-[#00000030] z-40 flex items-center justify-center">
-          <div className="w-[90%] 800px:w-[60%] h-[90vh] overflow-y-scroll 800px:h-[75vh] bg-white rounded-md shadow-sm relative p-4">
+          <div className="w-[90%] 800px:w-[70%] h-[90vh] overflow-y-scroll 800px:h-[75vh] bg-white rounded-md shadow-sm relative p-4 pl-6 pr-2">
             <RxCross1
               size={30}
               className="absolute right-3 top-3 z-50 "
               onClick={() => setOpen(false)}
             />
             <div className="block w-full 800px:flex">
-              <div className="w-full 800px:w-[50%] ">
+              <div className="w-full 800px:w-[45%] ">
                 <img src={data.image_Url[0].url} alt="" />
-                <div className="flex">
+                <div className="flex mt-6">
                   <img
                     src={data.shop.shop_avatar.url}
                     className="w-[50px] h-[50px] rounded-full mr-2"
@@ -36,7 +41,7 @@ const ProductDetailsCard = ({ open, setOpen, data }) => {
                   </div>
                 </div>
                 <div
-                  className={`${styles.button} bg-[#000] mt-4 rounded-[4px] h-11`}
+                  className={`${styles.button} bg-[#000] mt-4 rounded-[3px] h-11`}
                   onClick={handleMessageSubmit}
                 >
                   <span className="text-white flex items-center">
@@ -53,6 +58,65 @@ const ProductDetailsCard = ({ open, setOpen, data }) => {
                   {data.name}
                 </h1>
                 <p>{data.description}</p>
+                <div className="flex pt-3">
+                  <h4 className={`${styles.productDiscountPrice}`}>
+                    {data.discount_price}$
+                  </h4>
+                  <h3 className={`${styles.price}`}>
+                    {data.price ? data.price + "$" : null}
+                  </h3>
+                </div>
+                <div className="flex items-center mt-12 justify-between pr-3">
+                  <div>
+                    <button
+                      onClick={() => {
+                        if (count > 1) setCount(count - 1);
+                      }}
+                      className="bg-gradient-to-r from-teal-400 to-teal-500 text-white font-bold rounded-l px-4 py-2 shadow-lg hover:opacity-75 transition- duration-300 ease-in-out
+              "
+                    >
+                      -
+                    </button>
+                    <span className="bg-gray-200 text-gray-800 font-medium px-4 py-[11px]  ">
+                      {count}
+                    </span>
+                    <button
+                      onClick={() => {
+                        setCount(count + 1);
+                      }}
+                      className="bg-gradient-to-r from-teal-400 to-teal-500 text-white font-bold rounded-l px-4 py-2 shadow-lg hover:opacity-75 transition- duration-300 ease-in-out
+              "
+                    >
+                      +
+                    </button>
+                  </div>
+                  <div>
+                    {click ? (
+                      <AiFillHeart
+                        size={30}
+                        className="cursor-pointer"
+                        color={click ? "red" : "#333"}
+                        onClick={() => setClick(!click)}
+                        title="Remove from wishlist"
+                      />
+                    ) : (
+                      <AiOutlineHeart
+                        size={30}
+                        className="cursor-pointer "
+                        color={click ? "red" : "#333"}
+                        onClick={() => setClick(!click)}
+                        title="Add to wishlist"
+                      />
+                    )}
+                  </div>
+                </div>
+                <div
+                  className={`${styles.button} mt-6 rounded-[3px] h-11 flex items-start`}
+                >
+                  <span className="text-white flex items-center ">
+                    Add to Cart <AiOutlineShoppingCart className="ml-1" />
+                  </span>
+                </div>
               </div>
             </div>
           </div>
