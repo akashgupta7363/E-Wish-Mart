969a9dd21f5a3ce6d3644a1f94866e3e412161fc
Akash Gupta <akashgupta7363@gmail.com>
2023-06-04 12:26:17 +0530

    deploy code server

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "http://ewishmart.netlify.app",
     credentials: true,
   })
 );
@@ -26,6 +26,9 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 //routes import
 const user = require("./controller/userControllers");
 
+app.get("/", (req, res) => {
+  res.json("API is running").status(200);
+});
 app.use("/api/v2/user", user);
 
 app.use(ErrorHandler);
--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -9,6 +9,8 @@ const sendToken = (user, statusCode, res) => {
   const options = {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
     httpOnly: true,
+    sameSite: "none",
+    secure: true,
   };
   res.status(statusCode).cookie("token", token, options).json({
     success: true,
