95d281235d672cdf9bbfd85f698040d39037aa62
Akash Gupta <akashgupta7363@gmail.com>
2023-06-04 10:15:30 +0530

    axios

--- frontend/src/redux/actions/user.js
+++ frontend/src/redux/actions/user.js
@@ -2,6 +2,8 @@ import {} from "@reduxjs/toolkit";
 import axios from "axios";
 import { server } from "../../server";
 
+axios.defaults.withCredentials = true;
+
 export const loadUser = () => async (dispatch) => {
   try {
     dispatch({
