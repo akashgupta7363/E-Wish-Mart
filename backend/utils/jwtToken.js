22a31c5c618ece4d90683507be78bf65f67061be
Akash Gupta <akashgupta7363@gmail.com>
2023-06-04 13:08:28 +0530

    jwt

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -9,7 +9,7 @@ const sendToken = (user, statusCode, res) => {
   const options = {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    sameSite: "none",
+    sameSite: "None",
     secure: true,
   };
   res
