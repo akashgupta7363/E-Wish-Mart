bc0e00817d9f8bf8b9d21c281aa96392ad6188a4
Akash Gupta <akashgupta7363@gmail.com>
2023-06-04 10:44:50 +0530

    cookieoptions

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -9,6 +9,8 @@ const sendToken = (user, statusCode, res) => {
   const options = {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
     httpOnly: true,
+    sameSite: "None",
+    secure: true,
   };
   res
     .header("Access-Control-Allow-Origin", "https://e-wish-mart.onrender.com")
