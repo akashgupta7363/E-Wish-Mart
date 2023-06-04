d2e3fc64f67c8b6be9a477e124da29dbcd9393b2
Akash Gupta <akashgupta7363@gmail.com>
2023-06-04 12:35:29 +0530

    client side deploying code

--- backend/controller/userControllers.js
+++ backend/controller/userControllers.js
@@ -39,7 +39,7 @@ router.post("/create-user", upload.single("file"), async (req, res, next) => {
 
     const activationToken = createActiveToken(user);
 
-    const activationUrl = `http://localhost:3000/activation/${activationToken}`;
+    const activationUrl = `http://ewishmart.netlify.app/activation/${activationToken}`;
     try {
       await sendMail({
         email: user.email,
--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,2 +1,2 @@
-export const server = "http://localhost:5000/api/v2";
-export const backend_url = "http://localhost:5000/";
+export const server = "https://ewishmartserver.onrender.com/api/v2";
+export const backend_url = "https://ewishmartserver.onrender.com/";
