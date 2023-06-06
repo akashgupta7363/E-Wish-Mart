7c6dd3fe7c5a99af59fc79f673f8959b71049679
Akash Gupta <akashgupta7363@gmail.com>
2023-06-06 19:49:05 +0530

    fix

--- frontend/src/components/Route/Events/CountDown.js
+++ frontend/src/components/Route/Events/CountDown.js
@@ -9,7 +9,7 @@ const CountDown = () => {
     return () => clearTimeout(timer);
   });
   function calculateTimeLeft() {
-    const difference = +new Date("2023-05-29") - +new Date();
+    const difference = +new Date("2023-06-19") - +new Date();
     let timeLeft = {};
     if (difference > 0) {
       timeLeft = {
